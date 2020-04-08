aba23c09ead6427b287e4e90dabd497e49107f96
schimar <schimar.com>
2020-04-08 19:29:26 +0200

    pca housekeeping

--- pca.py
+++ pca.py
@@ -38,56 +38,9 @@ sns.set_style('ticks')
 
 ######################################
 
-# get the data
-## (I'd suggest converting to zarr)
-
-# subs = al.read_vcf('subs85f1byPop.vcf', numbers= {'GT': 2, 'ALT': 1}, fields= '*')
-
-subsvcfPath = 'subs85f1byPop.vcf'
-subszarrPath = 'subs85f1byPop.zarr'
-
-# convert from vcf to zarr
-al.vcf_to_zarr(subsvcfPath, subszarrPath, fields='*', log=sys.stdout, overwrite=True)
-
-# read data
-# NOTE: in the future, you can simply do this, which is much faster and memory-efficient
-subs = zarr.open_group(subszarrPath, mode='r')
-
-# see what's in there:
-    subs.tree(expand= True)
-    # or, to show the main keys of the dict:
-    sorted(subs.keys())
 
 
-
-
-## read id file and perform some basic checks   (NOTE: this is a space-delimited file with columns [ids, nest, pops])
-ids = pd.read_csv('tapopsOrd.csv', delimiter= ' ')
-
-
-    # count number of inds per pop and nest
-    ids.groupby(by= ['nest', 'pops']).count()
-
-    # is the order of samples in ids and subs the same?
-    np.all(list(subs['samples']) == ids['id'].values)
-
-    # if not, then we can add a column to ids with the index in which they appear in subs:
-    samples = list(subs['samples'])
-    subsIndex = [samples.index(s) for s in ids['id']]
-    ids['subsIndex'] = subsIndex
-
-
-
-###############
-# create genotype array
-
-gtsub = al.GenotypeArray(subs['calldata/GT'])
-
-    # NOTE: you can simply get a boolean 2d-array of inds by heterozygous loci:
-    gtsub.is_het()
-
-
-# get id indices (for all, and the three respective pops [A, N, S])
+## get id indices (for all, and the three respective pops [A, N, S])
 # this is a dictionary, with the three keys [all, A, N, S] and values are lists of indices for each respective id
 subpops = {
     'all': list(range(len(ids))),
@@ -97,7 +50,7 @@ subpops = {
 }
 
 
-# count alleles
+## count alleles
 # this is also a dict, with the same keys as subpops [all, A, N, S] and each of the four 'values' is an AlleleCountsArray
 ac_subpops_subs = gtsub.count_alleles_subpops(subpops, max_allele=1)
 
@@ -105,7 +58,7 @@ ac_subpops_subs = gtsub.count_alleles_subpops(subpops, max_allele=1)
     np.count_nonzero((ac_subpops_subs['all'].max_allele() == 1) & ac_subpops_subs['all'].is_singleton(1))
 
 
-# locate segregating alleles and subset (which is a np 1d array - see segAll_subs.shape)
+## locate segregating alleles and subset (which is a np 1d array - see segAll_subs.shape)
 segAll_subs = ac_subpops_subs['all'].is_segregating()[:]
 
     # check segregating alleles per pop:
@@ -113,15 +66,15 @@ segAll_subs = ac_subpops_subs['all'].is_segregating()[:]
         print(pop, ac_subpops_subs[pop].count_segregating())
 
 
-# subset loci with segregating alleles
+## subset loci with segregating alleles
 # NOTE: we here create a new GenotypeArray
 gtseg_subs = gtsub.compress(segAll_subs, axis=0)
 
 
-# create matrix (or rather a np 2d array) with loci x individuals containing icontaining the number of alt alleles (0, 1 or 2)
+## create matrix (or rather a np 2d array) with loci x individuals containing icontaining the number of alt alleles (0, 1 or 2)
 nAltSub = gtseg_subs.to_n_alt()
 
-# check for LD (first 1000 loci)
+## check for LD (first 1000 loci)
 def plot_ld(gn, title):
     m = al.rogers_huff_r(gn) ** 2
     ax = al.plot_pairwise_ld(m)
@@ -130,7 +83,7 @@ def plot_ld(gn, title):
 plot_ld(nAltSub[:1000], 'Figure 1. Pairwise LD.')
 
 
-# random subsampling of loci
+## random subsampling of loci
 n = 100000  # number of SNPs to choose randomly
 vidx = np.random.choice(nAltSub.shape[0], n, replace=False)
 vidx.sort()
@@ -140,7 +93,7 @@ plot_ld(gnr[:1000], 'Figure 2. Pairwise LD after random downsampling.')
 
 
 
-# LD pruning    (i.e. sliding a window along the data, computing pairwise LD between all SNPs within each window, then removing one SNP from each correlated pair)
+## LD pruning    (i.e. sliding a window along the data, computing pairwise LD between all SNPs within each window, then removing one SNP from each correlated pair)
 def ld_prune(gn, size, step, threshold=.1, n_iter=1):
     for i in range(n_iter):
         loc_unlinked = al.locate_unlinked(gn, size=size, step=step, threshold=threshold)
@@ -158,7 +111,7 @@ plot_ld(gnu[:1000], 'Figure 3. Pairwise LD after LD pruning.')
 
 ###############
 
-# PCA using Singular Value Decomposition (SVD) and Patterson's scaling on LD-pruned data (see gnu.shape for dimensions)
+## PCA using Singular Value Decomposition (SVD) and Patterson's scaling on LD-pruned data (see gnu.shape for dimensions)
 coords1, model1 = al.pca(gnu, n_components=10, scaler='patterson')
 
 populations = ids.pops.unique()
@@ -199,24 +152,24 @@ fig_pca(coords1, model1, 'Figure 4. Conventional PCA.', pops = ids.pops, pcols= 
 
 
 
-# pca without LD pruning for the random subset of 100k loci with Patterson's scaling
+## pca without LD pruning for the random subset of 100k loci with Patterson's scaling
 coords2, model2 = al.pca(gnr, n_components=10, scaler='patterson')
 fig_pca(coords2, model2, 'Figure 5. Conventional PCA without LD pruning.', pops = ids.pops, pcols= pop_cols)
 
-# now for the full set (gtseg) with Patterson's scaling
+## now for the full set (gtseg) with Patterson's scaling
 #   NOTE: probably do not run this on your laptop
 coords2all, model2all = al.pca(nAltSub, n_components=10, scaler='patterson')
 fig_pca(coords2all, model2all, 'Conventional PCA without LD pruning.', pops = ids.pops, pcols= pop_cols)
 
-# pca + LD-pruning, without Patterson's scaling
+## pca + LD-pruning, without Patterson's scaling
 coords3, model3 = al.pca(gnu, n_components=10, scaler=None)
 fig_pca(coords3, model3, 'Figure 6. Conventional PCA without variance scaling.', pops = ids.pops, pcols= pop_cols)
 
-# randomized PCA with LD-pruning and Patterson's scaling
+## randomized PCA with LD-pruning and Patterson's scaling
 coords5, model5 = al.randomized_pca(gnu, n_components=10, scaler='patterson')
 fig_pca(coords5, model5, 'Figure 8. Randomized PCA.', pops = ids.pops, pcols= pop_cols)
 
-# pca with even sample sizes NOTE: not really needed here, see alimanfoo's Fast PCA site
+## pca with even sample sizes NOTE: not really needed here, see alimanfoo's Fast PCA site
 # (https://alimanfoo.github.io/2015/09/28/fast-pca.html)
 # also: see alimanfoo's Fast-PCA post on an evaluation of the lower PCs in randomized PCA
 
