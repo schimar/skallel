7912a62427fb1826283672e3994300b81c7012c7
schimar <schimar.com>
2020-04-08 12:06:41 +0200

    divergence first commit

---
+++ divergence.py
@@ -0,0 +1,26 @@
+#  minimal example for the estimation of diversity statistics from a vcf file
+
+# In this script, we do the following:
+
+
+#####################################
+
+# import random     # to set the seed
+#random.seed(42)
+import sys
+import allel as al
+import numpy as np
+#np.random.seed(42)
+import pandas as pd
+import zarr
+#import scipy.parse
+#import scipy.spatial
+%matplotlib
+import matplotlib.pyplot as plt
+#import matplotlib as mpl
+import seaborn as sns
+sns.set_style('white')
+sns.set_style('ticks')
+
+######################################
+
