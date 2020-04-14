33f0478503455ee658afb2943f6bad4947dac964
schimar <schimar.com>
2020-04-14 11:07:19 +0200

    first commit baseStats

---
+++ baseStats.py
@@ -0,0 +1,29 @@
+#  minimal example for a first inspection of our variants
+
+
+#       scaffolds
+#       variant density
+
+
+
+#####################################
+
+# import random     # to set the seed
+#random.seed(42)
+import sys
+import allel as al
+import numpy as np
+#np.random.seed(42)
+import pandas as pd
+import zarr
+#import scipy.parse
+#import scipy.spatial
+%matplotlib
+import matplotlib.pyplot as plt
+#import matplotlib as mpl
+import seaborn as sns
+sns.set_style('white')
+sns.set_style('ticks')
+
+######################################
+
